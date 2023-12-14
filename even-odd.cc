8127e008e8d7f10c5f262594c8948ff37ac2cd7c
Arpita Singh <151006850+arpitabkn3@users.noreply.github.com>
2023-12-14 19:34:04 +0530

    Create even-odd.cc

---
+++ even-odd.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+    int a;
+    cout<<"enter the number";
+    cin>>a;
+    if(a%2==0)
+    {
+        cout<<"number is even";
+    }
+    else
+    {
+        cout<<"number is odd";
+    }
+    
+    }
