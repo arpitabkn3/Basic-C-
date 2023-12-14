16a0e783315fa7e7b88024be3b4bef65a64b51d4
Arpita Singh <151006850+arpitabkn3@users.noreply.github.com>
2023-12-14 19:31:52 +0530

    Create pos-neg.cc

---
+++ pos-neg.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+    int a;
+    cout<<"enter the number";
+    cin>>a;
+    if(a>0)
+    {
+        cout<<"number is positive";
+    }
+    else if(a<0)
+    {
+        cout<<"number is negative";
+    }
+    
+    }
