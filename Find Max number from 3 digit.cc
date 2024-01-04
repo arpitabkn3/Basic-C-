481d1afeaa63fe9da7c88caa0642307aedaefe2e
Arpita Singh <151006850+arpitabkn3@users.noreply.github.com>
2024-01-05 01:13:14 +0530

    Find Max number from 3 digit.cc

---
+++ Find Max number from 3 digit.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b){
+        cout<<a<<endl;
+        
+    }else
+    {
+        if(b>c){
+            cout<<b<<endl;
+        }
+        else{
+            cout<<endl;
+        }
+    }}
